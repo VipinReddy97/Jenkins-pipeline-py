dde9fa2c877486c6e2d72d7be75f3d27baf76113
vipinreddy <69107820+VipinReddy97@users.noreply.github.com>
2021-04-08 23:27:11 +0530

    Updating main application

--- pycalculator.py
+++ pycalculator.py
@@ -3,8 +3,7 @@
 # This function adds two numbers
 def add(x, y):
     if x <= 0 or y <= 0:
-        print("Only positive integers allowed in this calculator:")
-        return
+        return "Only positive integers allowed in this calculator:"
     else:
         return x + y
 
@@ -12,8 +11,7 @@ def add(x, y):
 # This function subtracts two numbers
 def subtract(x, y):
     if x <= 0 or y <= 0:
-        print("Only positive integers allowed in this calculator:")
-        return
+        return "Only positive integers allowed in this calculator:"
     else:
         return x - y
 
@@ -21,34 +19,12 @@ def subtract(x, y):
 # This function multiplies two numbers
 def multiply(x, y):
     if x <= 0 or y <= 0:
-        print("Only positive integers allowed in this calculator:")
-        return
+        return "Only positive integers allowed in this calculator:"
     else:
         return x * y
 
 
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
 
 
-while True:
 
-    choice = input("Enter choice(1/2/3): ")
-# Check if choice is one of the four options
-    if choice in ('1', '2', '3'):
-        num1 = int(input("Enter first number: "))
-        num2 = int(input("Enter second number: "))
 
-        if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
-
-        elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
-
-        elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
-            break
-    else:
-        print("Invalid Input")
