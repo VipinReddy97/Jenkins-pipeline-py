1ba01ce85ccebf06c5cbc1128f2c6daede1d5886
vipinreddy <69107820+VipinReddy97@users.noreply.github.com>
2021-04-08 23:28:33 +0530

    updating unit test cases

--- pycalculator-unitest.py
+++ pycalculator-unitest.py
@@ -7,21 +7,23 @@ class MyTestCase(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(4, 5), 9)
         self.assertNotEqual(add(3, 7), 9)
-        msg = "error"
+        msg = "Only positive integers allowed in this calculator:"
         self.assertEqual(add(-3, 2), msg)
 
     def test_subtract(self):
         self.assertEqual(subtract(4, 5), -1)
         self.assertNotEqual(subtract(2, 1), 0)
-        msg = "error"
+        msg = "Only positive integers allowed in this calculator:"
         self.assertEqual(subtract(-5, 2), msg)
 
     def test_multiply(self):
         self.assertEqual(multiply(4, 5), 20)
         self.assertNotEqual(multiply(3, 7), 9)
-        msg = "error"
+        msg = "Only positive integers allowed in this calculator:"
         self.assertEqual(multiply(-3, -2), msg)
 
 
 if __name__ == '__main__':
     unittest.main()
+
+
