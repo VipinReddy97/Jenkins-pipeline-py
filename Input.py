854f5b82970b130606d121d22dee004187bbcad6
vipinreddy <69107820+VipinReddy97@users.noreply.github.com>
2021-04-13 14:30:26 +0530

    Update Input.py

--- Input.py
+++ Input.py
@@ -5,7 +5,7 @@ print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
-print("4.Divide")
+
 
 while True:
     # Take input from the user
@@ -13,8 +13,8 @@ while True:
 
     # Check if choice is one of the four options
     if choice in ('1', '2', '3', '4'):
-        num1 = int(input("Enter first number: "))
-        num2 = int(input("Enter second number: "))
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
 
         if choice == '1':
             print(num1, "+", num2, "=", add(num1, num2))
@@ -24,9 +24,7 @@ while True:
 
         elif choice == '3':
             print(num1, "*", num2, "=", multiply(num1, num2))
-
-        elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
         break
+        
     else:
         print("Invalid Input")
