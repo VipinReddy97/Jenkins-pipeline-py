14938c7d0a3e90d0e8565e43c6a58f06300eb576
vipinreddy <69107820+VipinReddy97@users.noreply.github.com>
2021-08-17 16:16:41 +0530

    Updating Code

--- pycalculator-unitest.py
+++ pycalculator-unitest.py
@@ -7,20 +7,17 @@ class MyTestCase(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(4, 5), 9)
         self.assertNotEqual(add(3, 7), 9)
-        msg = "Only positive integers allowed in this calculator:"
-        self.assertEqual(add(-3, 2), msg)
+        
 
     def test_subtract(self):
         self.assertEqual(subtract(4, 5), -1)
         self.assertNotEqual(subtract(2, 1), 0)
-        msg = "Only positive integers allowed in this calculator:"
-        self.assertEqual(subtract(-5, 2), msg)
+        
 
     def test_multiply(self):
         self.assertEqual(multiply(4, 5), 20)
         self.assertNotEqual(multiply(3, 7), 9)
-        msg = "Only positive integers allowed in this calculator:"
-        self.assertEqual(multiply(-3, -2), msg)
+        
 
 
 if __name__ == '__main__':
